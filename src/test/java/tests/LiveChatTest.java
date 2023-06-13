cc8e7c2c61c0d5e3ff1072a70258b6fef0338cc8
Vladyslav <133350975+vladyslavsydun@users.noreply.github.com>
2023-06-13 17:41:17 +0300

    Update LiveChatTest.java

--- src/test/java/tests/LiveChatTest.java
+++ src/test/java/tests/LiveChatTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import base.AbstractBaseTest;
-import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
@@ -12,9 +11,11 @@ public class LiveChatTest extends AbstractBaseTest {
     @Test
     public void messagingLiveChat() throws InterruptedException {
         HomePage homePage = new HomePage(driver);
+      
         homePage.clickBtnLiveChat();
         homePage.liveChatInsertMessage("Good afternoon, i want to buy some goods in your store. Can you help me with it?");
         homePage.clickliveChatSendButton();
+        
         assertTrue(homePage.getLiveChatAnswerOne().isDisplayed(), "Answer one is not displayed");
         assertTrue(homePage.getLiveChatAnswerTwo().isDisplayed(), "Answer one is not displayed");
     }
